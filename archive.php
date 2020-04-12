5934b0ee7e65d0acb519f49b016f921abde670da
Chris Yang <chrisyx511@gmail.com>
2020-04-12 15:30:11 -0400

    fixed display issues

--- archive.php
+++ archive.php
@@ -1,35 +1,36 @@
 <?php get_header();?>
 
 
-<div class="rectangle"></div>
-<section class="page-wrap">
-    <div class='container'>
-                 <h1>All Posts</h1>
-                 <?php get_template_part('includes/section', 'archive');?>
-                <div class='pagination'>
-                    
-                    <?php
-                        function paginate() {
-                        global $wp_query;
-
-                        $big = 999999999999;
-
+<div class="rectangle">
+    <section class="page-wrap">
+        <div class='container'>
+                    <h1>All Posts</h1>
+                    <?php get_template_part('includes/section', 'archive');?>
+                    <div class='pagination'>
                         
-                        echo get_the_posts_pagination(
-                            array (
-                                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                                'format' => '?paged=%#%',
-                                'current' => max(1, get_query_var('paged')),
-                                'total' => $wp_query->max_num_pages 
-                            )
-                        );
-                        
-                    }
-                    paginate();
-                    ?>
-                </div>
-     </div>
-</section>
+                        <?php
+                            function paginate() {
+                            global $wp_query;
+
+                            $big = 999999999999;
+
+                            
+                            echo get_the_posts_pagination(
+                                array (
+                                    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                                    'format' => '?paged=%#%',
+                                    'current' => max(1, get_query_var('paged')),
+                                    'total' => $wp_query->max_num_pages 
+                                )
+                            );
+                            
+                        }
+                        paginate();
+                        ?>
+                    </div>
+        </div>
+    </section>
+</div>
 
 
 <?php get_footer();?>
