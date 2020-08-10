255a43a4f96852902320913b127f3c32473b968f
Chris Yang <58494380+ChrisYx511@users.noreply.github.com>
2020-08-10 15:48:31 -0400

    fix logo

--- header.php
+++ header.php
@@ -6,8 +6,19 @@
     <title><?php echo get_bloginfo('name', 'raw')?></title>
 
     <?php wp_head();?>
-    <div class=custom-logo> 
-        <?php the_custom_logo()?>
+    <div class=custom> 
+        <div class=custom-logo>
+            <?php the_custom_logo()?>
+        </div>
+        <div class=logo-line>
+            <img alt=the-line src=<?php echo get_template_directory_uri()?>/images/line.svg></img>
+        </div>
+        <div class=custom-title>
+            <div id=title-text>
+            <h5><?php echo get_bloginfo('name', 'raw'); ?> </h5>
+            <p><?php echo get_bloginfo('description', 'raw'); ?></p>
+         </div>
+        </div>
     </div>
 </head>
 <body>
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form class="form-inline" action="/" method="get">
 
-    <input class="form-control mr-sm-2"type="search" name="s" placeholder="Search" aria-label="Search" id="search" value="<?php the_search_query();?>"
+    <input class="form-control mr-sm-2"type="search" name="s" placeholder="Potatoes with cheese" aria-label="Search" id="search" value="<?php the_search_query();?>"
      required>
     
      <button type="submit" class="btn btn-outline-primary my-2 my-sm-0">Search!</button>
